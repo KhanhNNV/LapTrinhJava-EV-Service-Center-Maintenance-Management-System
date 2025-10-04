da097ffdb11b37e90be6d0171ab37886b8558d78
Binh <dovanbinh1512@gmail.com>
2025-10-04 17:18:24 +0700

    update invoice

---
+++ EVService/src/main/java/edu/uth/evservice/EVService/services/InvoiceService.java
@@ -0,0 +1,32 @@
+package edu.uth.evservice.EVService.services;
+
+import edu.uth.evservice.EVService.model.Invoice;
+import edu.uth.evservice.EVService.repositories.InvoiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class InvoiceService {
+
+    @Autowired
+    private InvoiceRepository invoiceRepository;
+
+    public List<Invoice> getAllInvoices() {
+        return invoiceRepository.findAll();
+    }
+
+    public Optional<Invoice> getInvoiceById(int id) {
+        return invoiceRepository.findById(id);
+    }
+
+    public Invoice saveInvoice(Invoice invoice) {
+        return invoiceRepository.save(invoice);
+    }
+
+    public void deleteInvoice(int id) {
+        invoiceRepository.deleteById(id);
+    }
+}
