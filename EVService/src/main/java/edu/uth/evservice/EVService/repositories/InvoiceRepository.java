2a451c49fbc437fb6a668a0922554a8fbde781c5
Binh <dovanbinh1512@gmail.com>
2025-10-04 17:05:47 +0700

    code invoice

---
+++ EVService/src/main/java/edu/uth/evservice/EVService/repositories/InvoiceRepository.java
@@ -0,0 +1,10 @@
+package edu.uth.evservice.EVService.repositories;
+
+import edu.uth.evservice.EVService.model.Invoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
+    // Nếu muốn thêm method tùy chỉnh, viết ở đây
+}
