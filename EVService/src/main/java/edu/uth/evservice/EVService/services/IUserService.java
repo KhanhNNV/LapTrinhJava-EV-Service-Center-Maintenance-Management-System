8841da54c8f2a08a44c3c1964f46c530d2e375f1
Manh_Dev <manhnguyen0865280164@gmail.com>
2025-10-29 11:18:23 +0700

    xử lí đăng nhập bằng username hoặc password

--- EVService/src/main/java/edu/uth/evservice/EVService/services/IUserService.java
+++ EVService/src/main/java/edu/uth/evservice/EVService/services/IUserService.java
@@ -1,14 +1,29 @@
 package edu.uth.evservice.EVService.services;
 
-import edu.uth.evservice.EVService.dto.UserDto;
-import edu.uth.evservice.EVService.requests.CreateUserRequest;
-
 import java.util.List;
+import java.util.Optional;
+
+import edu.uth.evservice.EVService.dto.UserDto;
+import edu.uth.evservice.EVService.model.User;
+import edu.uth.evservice.EVService.requests.CreateUserRequest;
 
 public interface IUserService {
     UserDto createUser(CreateUserRequest request);
+
     UserDto getUserById(Integer id);
+
     List<UserDto> getAllUsers();
+
     UserDto updateUser(Integer id, CreateUserRequest request);
+
     void deleteUser(Integer id);
+
+    /**
+     * Tìm kiếm một User entity dựa trên username.
+     * Trả về Optional<User> để xử lý trường hợp không tìm thấy một cách an toàn.
+     * 
+     * @param username Tên đăng nhập cần tìm.
+     * @return Optional chứa User nếu tìm thấy, ngược lại là Optional rỗng.
+     */
+    Optional<User> findByUsername(String username);
 }
