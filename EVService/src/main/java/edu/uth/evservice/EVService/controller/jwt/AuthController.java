037fd0733ca8096538e1624dc05800bad8c2465f
Manh_Dev <manhnguyen0865280164@gmail.com>
2025-10-29 11:19:55 +0700

    Revert "xử lí đăng nhập bằng username hoặc password"
    This reverts commit 8841da54c8f2a08a44c3c1964f46c530d2e375f1.

--- EVService/src/main/java/edu/uth/evservice/EVService/controller/jwt/AuthController.java
+++ EVService/src/main/java/edu/uth/evservice/EVService/controller/jwt/AuthController.java
@@ -1,5 +1,6 @@
 package edu.uth.evservice.EVService.controller.jwt;
 
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import edu.uth.evservice.EVService.dto.UserDto;
 import edu.uth.evservice.EVService.dto.jwt.JwtDto;
@@ -20,6 +22,7 @@ import edu.uth.evservice.EVService.services.IUserService;
 import edu.uth.evservice.EVService.services.jwt.IJwtService;
 import lombok.RequiredArgsConstructor;
 
+
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
@@ -28,48 +31,47 @@ public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final IJwtService jwtService;
     private final IUserService userService;
-
-    // . API ĐĂNG NHẬP
+    
+    //. API ĐĂNG NHẬP
     @PostMapping("/login")
-    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest login) {
-        // ~Tạo đối tượng UsernamePasswordAuthenticationToken từ email và password người
-        // dùng gửi lên
+    public ResponseEntity<?> authenticateUser (@RequestBody LoginRequest login){
+        //~Tạo đối tượng UsernamePasswordAuthenticationToken từ email và password người dùng gửi lên
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                login.getUsernameOrEmail(),
-                login.getPassword());
-        // ~ Thực hiện xác thực bằng AuthenticationManager
+                login.getEmail(),
+                login.getPassword()
+        );
+        //~ Thực hiện xác thực bằng AuthenticationManager
         Authentication authentication = authenticationManager.authenticate(authToken);
-        // ~ Này viết ra để kiểm tra lỗi
+        //~ Này viết ra để kiểm tra lỗi
         Authentication authFromContext = SecurityContextHolder.getContext().getAuthentication();
         if (authFromContext != null && authFromContext.isAuthenticated()) {
             System.out.println("Người dùng '" + authFromContext.getName() + "' vừa đăng nhập thành công.");
         }
-        // ~ Tạo accessToken và refeshToken
+        //~ Tạo accessToken và refeshToken
         String accessToken = jwtService.generateAccessToken(authentication);
         String refreshToken = jwtService.generateRefeshToken(authentication);
-        // ~Trả 2 token về phía client
-        return ResponseEntity.ok(new JwtDto(accessToken, refreshToken));
+        //~Trả 2 token về phía client
+        return ResponseEntity.ok(new JwtDto(accessToken,refreshToken));
     };
 
-    // . API ĐĂNG KÝ
+    //. API ĐĂNG KÝ
     @PostMapping("/register")
-    public ResponseEntity<?> registerUser(@RequestBody RegisterRequest register) {
-        // ~ Chuyển thằng registerRequest sang thằng createUserRequest
+    public ResponseEntity<?> registerUser (@RequestBody RegisterRequest register){
+        //~ Chuyển thằng registerRequest sang thằng createUserRequest
         CreateUserRequest createUserRequest = new CreateUserRequest();
         createUserRequest.setUsername(register.getUsername());
         createUserRequest.setFullName(register.getFullName());
         createUserRequest.setEmail(register.getEmail());
-        createUserRequest.setPassword(register.getPassword()); // fix
+        createUserRequest.setPassword(register.getPassword());
         createUserRequest.setPhoneNumber(register.getPhoneNumber());
         createUserRequest.setAddress(register.getAddress());
         createUserRequest.setRole(Role.CUSTOMER.name());
         try {
             UserDto newUser = userService.createUser(createUserRequest);
             System.out.println("Đăng ký thành công" + newUser.getUserId());
-            return ResponseEntity
-                    .ok("Đăng ký thành công! User ID: " + newUser.getUserId() + "User email: " + newUser.getEmail());
+            return ResponseEntity.ok("Đăng ký thành công! User ID: " + newUser.getUserId()+ "User email: " + newUser.getEmail());
         } catch (RuntimeException e) {
-            System.out.println("Đăng ký người dùng thất bại! ");
+             System.out.println("Đăng ký người dùng thất bại! ");
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
--- EVService/src/main/java/edu/uth/evservice/EVService/repositories/IUserRepository.java
+++ EVService/src/main/java/edu/uth/evservice/EVService/repositories/IUserRepository.java
@@ -1,22 +1,15 @@
 package edu.uth.evservice.EVService.repositories;
-
-import java.util.Optional;
-
+import edu.uth.evservice.EVService.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import edu.uth.evservice.EVService.model.User;
+import java.util.Optional;
 
 @Repository
-public interface IUserRepository extends JpaRepository<User, Integer> {
+public interface IUserRepository extends JpaRepository<User,Integer> {
     Optional<User> findByUsername(String username);
-
-    Optional<User> findByUsernameOrEmail(String username, String email);
-
-    // . Đăng nhập bằng email, tìm kiếm không bị trùng email
+    //. Đăng nhập bằng email, tìm kiếm không bị trùng email
     Optional<User> findByEmail(String email);
-
     boolean existsByEmail(String email);
-
     boolean existsByUsername(String username);
 }
