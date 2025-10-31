ffd950dde9a402d4bcb5f46e9304d215aa74917d
Thethien2k5 <thienobita0203@gmail.com>
2025-10-31 14:06:47 +0700

    fix claim role

--- EVService/src/main/java/edu/uth/evservice/EVService/controller/VehicleController.java
+++ EVService/src/main/java/edu/uth/evservice/EVService/controller/VehicleController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/vehicles")
+@PreAuthorize("hasAnyRole('CUSTOMER')")
 @RequiredArgsConstructor
 public class VehicleController {
 
