bd7b2444d5eae4f47d9eaf4f07a26e28c50ed870
Binh <dovanbinh1512@gmail.com>
2025-11-20 20:55:46 +0700

    Bắt buộc JSON trả về tên là "isRead"

--- EVService/src/main/java/edu/uth/evservice/dtos/NotificationDto.java
+++ EVService/src/main/java/edu/uth/evservice/dtos/NotificationDto.java
@@ -1,5 +1,6 @@
 package edu.uth.evservice.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import java.time.LocalDateTime;
 
@@ -13,6 +14,7 @@ public class NotificationDto {
     private Integer userId;
     private String title;
     private String message;
+   @JsonProperty("isRead") //Bắt buộc JSON trả về tên là "isRead"
     private boolean isRead;
     private LocalDateTime createdAt;
 }
